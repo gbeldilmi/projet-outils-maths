35a02c97e87069216fcd9c5cbe2d30ed4e6fffd2
Guillaume BELDILMI <gbeldilmi@pm.me>
2022-09-27 11:39:46 +0200

    Update fourrier.py

--- fourrier.py
+++ fourrier.py
@@ -83,6 +83,7 @@ def dd(N, deform = False):
         for j in range(N):
             if b[i][j] != bbb[i][j]:
                 print("Error at position", i, j, "expected", b[i][j], "got", bbb[i][j])
+    bb[0, 0] = 0
     plt.figure("Test 2D")
     plt.subplot(3, 1, 1)
     plt.imshow(b)
@@ -93,12 +94,100 @@ def dd(N, deform = False):
     plt.subplot(3, 1, 3)
     plt.imshow(np.abs(bbb))
     plt.title("IFFT")
-    plt.show()    
+    plt.show()
+
+def img(path, N = -1):
+    """Test fft on custom image converted to grayscale"""
+    img = plt.imread(path)
+    i = np.dot(img[...,:3], [0.299, 0.587, 0.114])
+    ii = fft2d(i)
+    iii = ifft2d(ii)
+    print("Original image: ", i)
+    print("FFT: ", ii)
+    if N != -1:
+        for j in range(i.shape[0]):
+            for k in range(i.shape[1]):
+                if j+k > N:
+                    ii[j, k] = 0
+    print("IFFT: ", iii)
+    ii[0, 0] = 0
+    plt.figure("Test gray")
+    plt.subplot(3, 1, 1)
+    plt.imshow(i)
+    plt.title("Original image")
+    plt.subplot(3, 1, 2)
+    plt.imshow(np.abs(ii))
+    plt.title("FFT")
+    plt.subplot(3, 1, 3)
+    plt.imshow(np.abs(iii))
+    plt.title("IFFT")
+    plt.show()
+
+def rgb(path, N = -1):
+    """Test fft on each channel of a custom image"""
+    img = plt.imread(path)
+    r = img[:, :, 0]
+    g = img[:, :, 1]
+    b = img[:, :, 2]
+    rr = fft2d(r)
+    gg = fft2d(g)
+    bb = fft2d(b)
+    rrr = ifft2d(rr)
+    ggg = ifft2d(gg)
+    bbb = ifft2d(bb)
+    print("Original image: ", img)
+    print("FFT: ", rr, gg, bb)
+    if N != -1:
+        for i in range(img.shape[0]):
+            for j in range(img.shape[1]):
+                if i+j > N:
+                    rr[i, j] = 0
+                    gg[i, j] = 0
+                    bb[i, j] = 0
+    print("IFFT (R): ", rrr)
+    print("IFFT (G): ", ggg)
+    print("IFFT (B): ", bbb)
+    nimg = np.zeros((img.shape[0], img.shape[1], 3))
+    nimg[:, :, 0] = rrr
+    nimg[:, :, 1] = ggg
+    nimg[:, :, 2] = bbb
+    rr[0, 0] = 0
+    gg[0, 0] = 0
+    bb[0, 0] = 0
+    plt.figure("Test rgb")
+    plt.subplot(3, 3, 1)
+    plt.imshow(img)
+    plt.title("Original image")
+    plt.subplot(3, 3, 4)
+    plt.imshow(np.abs(rr))
+    plt.title("FFT (R)")
+    plt.subplot(3, 3, 5)
+    plt.imshow(np.abs(gg))
+    plt.title("FFT (G)")
+    plt.subplot(3, 3, 6)
+    plt.imshow(np.abs(bb))
+    plt.title("FFT (B)")
+    plt.subplot(3, 3, 7)
+    plt.imshow(np.abs(rrr))
+    plt.title("IFFT (R)")
+    plt.subplot(3, 3, 8)
+    plt.imshow(np.abs(ggg))
+    plt.title("IFFT (G)")
+    plt.subplot(3, 3, 9)
+    plt.imshow(np.abs(bbb))
+    plt.title("IFFT (B)")
+    plt.subplot(3, 3, 3)
+    plt.imshow(nimg)
+    plt.title("Reconstructed image")
+    plt.show()
 
 if __name__ == '__main__':
-    N = 16
+    N = 12
     d(N)
     d(N, True)
     dd(N)
     dd(N, True)
-
+    img("lenna.png")
+    img("lenna.png", N)
+    rgb("lenna.png")
+    rgb("lenna.png", N)
